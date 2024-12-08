098a95a05cb9d19a7c2cfa1b6fe9033562d35679
DelphicOkami <mjw@lukos.co>
2024-12-08 11:38:35 +0000

    Sensiblize defaults

--- days/blank_test.go
+++ days/blank_test.go
@@ -17,9 +17,9 @@ func TestRunBlankSuite(t *testing.T) {
 }
 
 func (suite *BlankSuite) SetupTest() {
-	suite.ProvidedInput = []string{"190: 10 19", "3267: 81 40 27", "83: 17 5", "156: 15 6", "7290: 6 8 6 15", "161011: 16 10 13", "192: 17 8 14", "21037: 9 7 18 13", "292: 11 6 16 20"}
+	suite.ProvidedInput = []string{}
 }
 
 func (suite *BlankSuite) TestOneCasesPresented() {
-	assert.Equal(suite.T(), 14, days.DayBlankPart1(suite.ProvidedInput))
+	assert.Equal(suite.T(), 0, days.DayBlankPart1(suite.ProvidedInput))
 }
